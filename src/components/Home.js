f1a222adcaec3563997613ca1925e27f8c4f46f0
BanditHeelerPDX <kirk.alexander.steele@gmail.com>
2023-06-12 17:27:23 -0700

    components have a new home or at least a Home.js

---
+++ src/components/Home.js
@@ -0,0 +1,21 @@
+import React from "react";
+
+const Home = () => {
+  return (
+    <section id="home">
+      <div className="home-content">
+        <h2>introducing...</h2>
+        <p>
+          ...the man of the moment, if only this moment. The man you've come
+          here to learn about. The father of two, who has taken on web
+          development a little late in the game. The partner who cooks and codes
+          nearly simultaneously. The avid golfer who wishes that desire and
+          frequency translated into eagles and birdies. Me, myself and I. Kirk
+          Alexander Rizzi.
+        </p>
+      </div>
+    </section>
+  );
+};
+
+export default Home;
