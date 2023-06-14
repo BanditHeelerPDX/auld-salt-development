dfd17ddacd0548353fc5c80a0459397484b016eb
BanditHeelerPDX <kirk.alexander.steele@gmail.com>
2023-06-13 23:42:02 -0700

    customizing app.js and debugging all the live long day

--- src/App.js
+++ src/App.js
@@ -1,25 +1,33 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from "react";
+import Aboot from "./components/Aboot";
+import Contact from "./components/Contact";
+import Footer from "./components/Footer";
+import Header from "./components/Header";
+import Home from "./components/Home";
+import Resume from "./components/Resume";
+import Taboot from "./components/Taboot";
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+  const [currentSection, setCurrentSection] = useState("home");
 
-export default App;
+  const handleSectionChange = (section) => {
+    setCurrentSection(section);
+  };
+
+  return (
+    <>
+    <Header
+         activeSection={currentSection}
+         setActiveSection={handleSectionChange}
+       />
+    {currentSection === "home" && <Home />}
+    {currentSection === "aboot" && <Aboot />}
+    {currentSection === "contact" && <Contact />}
+    {currentSection === "taboot" && <Taboot />}
+    {currentSection === "resume" && <Resume />}
+    <Footer />
+    </>
+    );
+    }
+    
+    export default App;
