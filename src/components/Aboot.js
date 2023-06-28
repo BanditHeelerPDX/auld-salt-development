4b28da2ff20369a6f7e4cfeab1c71e37770f35f2
BanditHeelerPDX <kirk.alexander.steele@gmail.com>
2023-06-28 13:32:50 -0700

    wrestling with the intricacies of the english language

--- src/components/Aboot.js
+++ src/components/Aboot.js
@@ -37,7 +37,7 @@ const Aboot = () => {
             Born in Colorado, raised across the US by popular culture and some
             culture a little less than popular. Partner, Father, Athlete,
             Developer, Outdoorsman; equally aware of how much I know and how
-            much I still have left to learn.
+            much I have left to learn.
           </p>
         </div>
       </div>
